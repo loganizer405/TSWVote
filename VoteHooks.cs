46ad3e0f6c1fa9b69b35009db03307cafc001721
Simon <mkontent@gmail.com>
2014-04-30 21:17:22 +0400

    Add Success hook implementation

---
+++ VoteHooks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+
+namespace TSWVote
+{
+	public static class VoteHooks
+	{
+		private static event VoteSuccessHandler VoteSuccess;
+
+		internal static void InvokeVoteSuccess(TSPlayer Player)
+		{
+			if (VoteSuccess != null) VoteSuccess.Invoke(new VoteSuccessArgs(Player));
+		}
+
+		public static void VoteSuccessRegister(VoteSuccessHandler Handler)
+		{
+			VoteSuccess += Handler;
+		}
+
+		public static void VoteSuccessDeregister(VoteSuccessHandler Handler)
+		{
+			VoteSuccess -= Handler;
+		}
+	}
+
+	public delegate void VoteSuccessHandler(VoteSuccessArgs e);
+
+	public class VoteSuccessArgs : EventArgs
+	{
+		public TSPlayer Player;
+
+		public VoteSuccessArgs(TSPlayer player)
+		{
+			Player = player;
+		}
+	}
+}
